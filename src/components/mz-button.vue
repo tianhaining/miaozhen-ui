39b613b987d677550e8291e975b1a9cd616f7e76
tianhaining <tianhaining@miaozhen.com>
2017-12-19 16:50:58 +0800

    主要按钮

--- src/components/mz-button.vue
+++ src/components/mz-button.vue
@@ -1,7 +1,22 @@
 <template lang="html">
-  <div class="">
-    <button type="button" name="button" class="buttonStyle">秒针按钮</button>
-  </div>
+  <button :disabled="disabled" class="mz-button"
+    @click="handleClick"
+    :autofocus="autofocus"
+    :type="nativeType"
+    :class="[
+      type ? 'mz-button--' + type : '',
+      size ? 'mz-button--' + size : '',
+      {
+        'is-disabled': disabled,
+        'is-loading': loading,
+        'is-plain': plain
+      }
+    ]"
+  >
+    <i class="mz-icon-loading" v-if="loading"></i>
+    <i :class="'mz-icon-' + icon" v-if="icon && !loading"></i>
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
 </template>
 
 <script>
@@ -11,12 +26,55 @@ export default {
     return {
 
     }
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: String,
+    icon: {
+      type: String,
+      default: ''
+    },
+    nativeType: {
+      type: String,
+      default: 'button'
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    autofocus: Boolean
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    }
   }
 }
 </script>
 
 <style lang="less">
-.buttonStyle {
-  color: red;
+@color: #eb3332;
+@hoverColor: #ff4e4e;
+@activeColor: #d61918;
+.button-style (@background, @fontColor, @border){
+  background-color: @background;
+  color: @fontColor;
+  border: @border;
+  outline: none;
+}
+.mz-button {
+  padding: .5rem 1rem;
+  cursor: pointer;
+  &--primary {
+    .button-style(@color, white, 1px solid @color)
+  }
+  &--primary:hover {
+    .button-style(@hoverColor, white, 1px solid @hoverColor)
+  }
+  &--primary:active {
+    .button-style(@activeColor, white, 1px solid @activeColor)
+  }
 }
 </style>
